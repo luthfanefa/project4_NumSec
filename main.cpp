2278e1c0352d8e48e4a6b7adbc6babffc36921a4
luthfanefa <luthfan.gd@gmail.com>
2026-09-13 17:49:31 +0700

    13/9/26

--- main.cpp
+++ main.cpp
@@ -5,25 +5,53 @@
 */
 #include <iostream>
 #include <vector>
+#include <sstream>
+#include <string>
 
 using namespace std;
 
 int main () {
 
     vector <int> num ;
-    int prenum, exit = 0 ;
-    cout << "--NUMBER SEQUENCER--" << endl << endl ;
-    cout << "enter multiply number : " << endl
-         << "-" ;
-    cin >> prenum ; 
-    num.push_back(prenum);
-    cout << endl ;
- 
-    for (int i = 0 ; i < num.size() ; i ++) {
-        
-            cout << "attempt : " << i+1 << endl ;
-        
+    string prenum ;
+    cout << "NUMBER SEQUENCER-BUBBLE SORT METHOD" << endl;
+    cout << "-";
+    getline(cin, prenum);
+
+
+    stringstream convert(prenum);
+    int tempt ;
+    while (convert >> tempt){
+        num.push_back(tempt);
+    }
+
+    cout << endl << "PROCCESS" << endl << endl ;
+
+    int total_attempt = 0 ;
+    int n = num.size() ;
+
+    for (int i = 0 ; i < n-1 ; i++ ) {
+
+        bool s_swap = false ;
+
+        for (int j = 0 ; j < n - i - 1 ; j++) {
+
+            if (num[j] > num[j + 1]) {
+                swap(num[j], num[j + 1]);
+                s_swap = true ;
+            }
         }
+        if (s_swap) {
+                total_attempt++ ;
+                cout << "attempt : " << total_attempt << endl;
+                for (int j = 0 ; j < n ; j++) {
+                    cout << num [j] << " ";
+                }
+                cout << endl ;
+            }
+    }
+    cout << endl << "total attempt : " << total_attempt ;
+
 
   return 0 ;
 
